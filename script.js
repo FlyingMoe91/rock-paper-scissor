3a9924cf5e52307f4e879a126a0cdf5cf1d2b10e
Patrick <p.moe.mueller@gmail.com>
2021-10-13 21:55:25 +0100

    ADD script.js

--- script.js
+++ script.js
@@ -1,27 +1,54 @@
-const pcChoice = ['rock', 'paper', 'scissors'];
+let computerSelection;
+let playerSelection;
 
 function computerPlay() {
+  const pcChoice = ['rock', 'paper', 'scissors'];
   return pcChoice[Math.floor(Math.random() * pcChoice.length)];
 }
 
-  const computerSelection = computerPlay();
-  const playerSelection =prompt("Rock, Paper or Scissors? Choose wisely.").toLowerCase();
-
 function playRound() {
+  computerSelection = computerPlay();
+  playerSelection =prompt("Rock, Paper or Scissors? Choose wisely.").toLowerCase();
+
   if (playerSelection == "rock" && computerSelection == "scissors" || 
       playerSelection == "scissors" && computerSelection == "paper" ||
       playerSelection =="paper" && computerSelection =="rock") {
-    return "You Win! " + playerSelection + " beats " + computerSelection;
+    return `You Win this round! ${playerSelection} beats ${computerSelection}`;
   }else if(computerSelection == "rock" && playerSelection == "scissors" || 
            computerSelection == "scissors" && playerSelection == "paper" ||
            computerSelection =="paper" && playerSelection =="rock") {
-    return "You lose! " + computerSelection + " beats " + playerSelection;
+    return `You lose this round! ${computerSelection} beats ${playerSelection}`;
   }else if(playerSelection === computerSelection) {
     return "It's a tie!";
   }else {
     return "Choice unknown";
   }
 }
-console.log(playerSelection);
-console.log(computerSelection);
-console.log(playRound());
+
+function game() {
+
+  let playerScore = 0;
+  let computerScore = 0;
+
+  for (let round = 0; round < 3; round++) {
+    let playround = playRound();
+    console.log(playround);
+
+    if(playround == `You Win this round! ${playerSelection} beats ${computerSelection}`) {
+      playerScore++;
+    }else if(playround == `You lose this round! ${computerSelection} beats ${playerSelection}`){
+      computerScore++;
+    }
+  console.log("Player: " + playerScore + "  || Computer: " + computerScore);
+  }
+
+  if(playerScore === computerScore) {
+    console.log(`No Winner, No Loser. This game was a tie`)
+  }else if(playerScore > computerScore) {
+    console.log(`Cangrats" You've won this game with ${playerScore}:${computerScore}`)
+  }else{
+    console.log( `That didn't went so well. You lose this game with ${playerScore}:${computerScore}`)
+  }
+}
+
+game();
